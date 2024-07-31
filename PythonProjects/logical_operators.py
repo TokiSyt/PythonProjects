941671ecb114225b40762d89ae8f73019e0f8eab
Tiago Silva <tiagosyt@gmail.com>
2024-07-31 13:11:30 +0200

    #4 commit

---
+++ PythonProjects/logical_operators.py
@@ -0,0 +1,15 @@
+'''high_income = True
+good_credit = False
+
+if high_income and good_credit:
+  print("Eligible for loan")
+else:
+    print("Not eligible for loan")'''
+
+has_good_credit = True
+has_criminal_record = False
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+else:
+    print("Not eligible")
