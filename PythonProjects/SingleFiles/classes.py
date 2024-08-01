7ffd23eea76cea2816fb0a3d82ecfd28e7701fbf
TokiSyt <yuutokei30@gmail.com>
2024-08-01 17:51:54 +0200

    Classes and Constructors

---
+++ PythonProjects/SingleFiles/classes.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f'Hi, I am {self.name}!')
+
+
+toki = Person("Toki Turcek")
+toki.talk()
+
+bob = Person("Bob Makaia")
+bob.talk()
